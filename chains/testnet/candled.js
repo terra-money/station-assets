3d8e3dd1517414d5c33c5e7fd3d4d523d1066835
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-06 14:48:58 +0100

    format chains files

--- chains/testnet/candled.js
+++ chains/testnet/candled.js
@@ -4,7 +4,6 @@ module.exports = {
   coinType: '118',
   gasAdjustment: 1.75,
   gasPrices: { stake: 0.015 },
-  stake: 0.015,
   icon: 'https://purepng.com/public/uploads/large/candle-s9b.png',
   lcd: 'https://candle-lcd.candeago.dev',
   name: 'Candled',
--- chains/testnet/juno.js
+++ chains/testnet/juno.js
@@ -4,7 +4,6 @@ module.exports = {
   coinType: '118',
   gasAdjustment: 1.75,
   gasPrices: { ujunox: 0.015 },
-  ujunox: 0.015,
   icon: 'https://user-images.githubusercontent.com/545047/184516834-4b8190b0-0721-4778-b4fb-fa19ed3f9279.svg',
   lcd: 'https://juno-testnet-api.polkachu.com',
   name: 'Juno',
