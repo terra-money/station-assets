f9a0ee1a7156ea02e0cec92b21e87db4e7886964
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-10 19:07:52 +0100

    calculate IBC denoms

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const glob = require('glob')
+const glob = require('glob-promise')
+const { Buffer } = require('buffer')
+const { Hash } = require('@keplr-wallet/crypto')
 const fs = require('fs').promises
 
 ;(async () => {
@@ -6,44 +8,93 @@ const fs = require('fs').promises
   fs.mkdir('./build', { recursive: true })
 
   // convert chains to json
-  glob('./chains/*/*.js', async (_, files) => {
-    const chainsOutPath = './build/chains.json'
-    const chainsOut = {}
+  const chains = {}
+  const chainsOutPath = './build/chains.json'
 
-    files.forEach((file) => {
-      const [_, folder, network, fileName] = file.split('/')
+  const chainFiles = await glob('./chains/*/*.js')
 
-      if (typeof chainsOut[network] === 'undefined') {
-        chainsOut[network] = {}
-      }
+  chainFiles.forEach((file) => {
+    const [_, folder, network, fileName] = file.split('/')
 
-      const fullPath = `./${file}`
-      const chainData = require(fullPath)
-      chainsOut[network][chainData.chainID] = chainData
-    })
-    // Format the JSON with indentions before writing.
-    const jsonList = JSON.stringify(chainsOut, null, 2)
-    await fs.writeFile(chainsOutPath, jsonList)
+    if (typeof chains[network] === 'undefined') {
+      chains[network] = {}
+    }
+
+    const fullPath = `./${file}`
+    const chainData = require(fullPath)
+    chains[network][chainData.chainID] = chainData
   })
 
-    // convert coins to json
-    glob('./coins/*/*.js', async (_, files) => {
-      const coinsOutPath = './build/coins.json'
-      const coinsOut = {}
-  
-      files.forEach((file) => {
-        const [_, folder, network, fileName] = file.split('/')
-  
-        if (typeof coinsOut[network] === 'undefined') {
-          coinsOut[network] = {}
-        }
-  
-        const fullPath = `./${file}`
-        const coinData = require(fullPath)
-        coinsOut[network][coinData.token] = coinData
-      })
-      // Format the JSON with indentions before writing.
-      const jsonList = JSON.stringify(coinsOut, null, 2)
-      await fs.writeFile(coinsOutPath, jsonList)
+  // convert coins to json
+  const coinsOutPath = './build/coins.json'
+  const coinsOut = {}
+  const ibcDenomMapOutPath = './build/ibc_denoms.json'
+  const ibcDenomMapOut = {}
+
+  const coinsFile = await glob('./coins/*/*.js')
+
+  coinsFile.forEach((file) => {
+    const [_, folder, network, fileName] = file.split('/')
+
+    if (typeof coinsOut[network] === 'undefined') {
+      coinsOut[network] = {}
+    }
+    if (typeof ibcDenomMapOut[network] === 'undefined') {
+      ibcDenomMapOut[network] = {}
+    }
+
+    const fullPath = `./${file}`
+    const coinData = require(fullPath)
+    coinsOut[network][coinData.token] = coinData
+
+    // add IBC denom on Terra
+    coinData.chains.map((chainID) => {
+      if (chains[network][chainID].ibc) {
+        const ibcDenomOnTerra = calculateIBCDenom(
+          chains[network][chainID].ibc.toTerra,
+          coinData.token,
+        )
+        ibcDenomMapOut[network][ibcDenomOnTerra] = coinData.token
+
+        // add IBC denom on other chains
+        Object.values(chains[network]).forEach(({ chainID: chainID2 }) => {
+          if (!chains[network][chainID2].ibc || chainID === chainID2) return
+
+          const ibcDenomOnOther = calculateIBCDenom(
+            chains[network][chainID2].ibc.fromTerra,
+            ibcDenomOnTerra,
+          )
+          ibcDenomMapOut[network][ibcDenomOnOther] = coinData.token
+        })
+      } else if (chains[network][chainID].prefix === 'terra') {
+        // add IBC denom on other chains
+        Object.values(chains[network]).forEach(({ chainID: chainID2 }) => {
+          if (!chains[network][chainID2].ibc || chainID === chainID2) return
+
+          const ibcDenomOnOther = calculateIBCDenom(
+            chains[network][chainID2].ibc.fromTerra,
+            coinData.token,
+          )
+          ibcDenomMapOut[network][ibcDenomOnOther] = coinData.token
+        })
+      }
     })
+  })
+
+  // Format the JSON with indentions before writing.
+  const jsonList = JSON.stringify(chains, null, 2)
+  await fs.writeFile(chainsOutPath, jsonList)
+  const coinsList = JSON.stringify(coinsOut, null, 2)
+  await fs.writeFile(coinsOutPath, coinsList)
+  const ibcList = JSON.stringify(ibcDenomMapOut, null, 2)
+  await fs.writeFile(ibcDenomMapOutPath, ibcList)
 })()
+
+function calculateIBCDenom(sourceChannel, denom) {
+  return (
+    'ibc/' +
+    Buffer.from(Hash.sha256(Buffer.from(`transfer/${sourceChannel}/${denom}`)))
+      .toString('hex')
+      .toUpperCase()
+  )
+}
