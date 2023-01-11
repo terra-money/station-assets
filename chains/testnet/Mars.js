e3ff31e9041fe28317af89de01c78aaf5671bb39
dancreee <dancreee@users.noreply.github.com>
2023-01-11 09:51:00 +0000

    Add

---
+++ chains/testnet/Mars.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: '4. ares-1',
+  lcd: 'https://testnet-rest.marsprotocol.io/',
+  gasAdjustment: 1.75,
+  gasPrices: { umars: 0 },
+  prefix: 'mars',
+  coinType: '330',
+  baseAsset: 'umars',
+  name: 'Mars Hub',
+  icon: 'https://marsprotocol.io/mars_logo_colored.svg',
+  ibc: {
+    fromTerra: '123',
+    toTerra: '123',
+  }
+}
