6c79099035525a0d9412912c73df8ff8ba8a90d2
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-11 10:48:18 +0100

    Add LocalTerra

---
+++ chains/localterra/localterra.js
@@ -0,0 +1,11 @@
+module.exports = {
+  chainID: 'localterra',
+  lcd: 'http://localhost:1317',
+  gasAdjustment: 1.75,
+  gasPrices: { uluna: 0.15 },
+  prefix: 'terra',
+  coinType: '330',
+  baseAsset: 'uluna',
+  name: 'LocalTerra',
+  icon: 'https://assets.terra.money/icon/svg/Terra.svg',
+}
