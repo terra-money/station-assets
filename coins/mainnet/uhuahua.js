a9ae2dde7a7fe583753440e64d8d20428ab2b490
Joshua Brigati <brigati.joshua@gmail.com>
2023-01-13 14:42:27 -0600

    change icon image endpoint.
    Saved as svg but is a ong

--- coins/mainnet/uhuahua.js
+++ coins/mainnet/uhuahua.js
@@ -2,7 +2,7 @@ module.exports = {
   token: 'uhuahua',
   symbol: 'HUAHUA',
   name: 'Huahua',
-  icon: 'https://station-assets.terra.money/img/coins/Huahua.svg',
+  icon: 'https://station-assets.terra.money/img/coins/Huahua.png',
   chains: ['chihuahua-1'],
   decimals: 8,
 }
