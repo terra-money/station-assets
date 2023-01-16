411f953c5b3d888f6332e67a60f3b8669866992c
emidev98 <emilian@terra.money>
2023-01-16 13:06:21 +0200

    feat: delete candled chain and import alliance chain

---
+++ chains/testnet/alliance.js
@@ -0,0 +1,11 @@
+module.exports = {
+  chainID: 'alliance-testnet-1',
+  lcd: 'http://ec2-3-75-187-158.eu-central-1.compute.amazonaws.com:1317/',
+  gasAdjustment: 2.5,
+  gasPrices: { stake: 0.0 },
+  prefix: 'alliance',
+  coinType: '118',
+  baseAsset: 'stake',
+  name: 'Alliance',
+  icon: 'http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/handshake.png'
+}
