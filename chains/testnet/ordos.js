799118c6a6c240cc9ab3b74623c707087c8049ba
javiersuweijie <javier.su.weijie@gmail.com>
2023-01-16 21:07:15 +0800

    feat: added corrino, ordos. updated harkonnen

---
+++ chains/testnet/ordos.js
@@ -0,0 +1,21 @@
+module.exports = {
+  chainID: "ordos-1",
+  lcd: "https://ordos-lcd.feather.network/",
+  gasAdjustment: 2.5,
+  gasPrices: { uord: 0.0 },
+  prefix: "ordos",
+  coinType: "118",
+  baseAsset: "uord",
+  name: "Ordos",
+  icon: "https://station-assets.terra.money/img/chains/Ordos.png",
+  ibc: {
+    toTerra: "channel-2",
+    fromTerra: "channel-200",
+    ics: {
+      contract:
+        "ordos1yyca08xqdgvjz0psg56z67ejh9xms6l436u8y58m82npdqqhmmtq56pc9e",
+      toTerra: "channel-1",
+      fromTerra: "channel-199",
+    },
+  },
+};
