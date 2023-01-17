e669e65467dbdf659e4da1e5a04b62ae6620700f
Javier Su <javier.su.weijie@gmail.com>
2023-01-17 18:55:04 +0800

    Update ordos.js

--- chains/testnet/ordos.js
+++ chains/testnet/ordos.js
@@ -21,7 +21,7 @@ module.exports = {
        contract:
         "terra12havy0g3svy6jlnc0je3z4f0lujhkp8h79ux5myhy4ujufeczpzsr5w7lz",
       toTerra: "channel-1",
-      fromTerra: "channel-4",
+      fromTerra: "channel-202",
     }
   },
 };
