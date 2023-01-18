696845d741840b3e685b4825328e61b4f550fc4e
Javier Su <javier.su.weijie@gmail.com>
2023-01-18 16:40:11 +0800

    Update corrino.js

--- chains/testnet/corrino.js
+++ chains/testnet/corrino.js
@@ -9,13 +9,13 @@ module.exports = {
   name: "Corrino",
   icon: "https://station-assets.terra.money/img/chains/Corrino.png",
   ibc: {
-    toTerra: "channel-1",
-    fromTerra: "channel-197",
+    toTerra: "channel-3",
+    fromTerra: "channel-204",
     ics: {
       contract:
         "corrino1yyca08xqdgvjz0psg56z67ejh9xms6l436u8y58m82npdqqhmmtqmxgcku",
-      toTerra: "channel-0",
-      fromTerra: "channel-196",
+      toTerra: "channel-2",
+      fromTerra: "channel-203",
     },
   },
 };
