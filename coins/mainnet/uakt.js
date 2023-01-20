648743ba106b7fbb6011d5a5b16d3cabe533e9d1
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-20 17:37:18 +0100

    Fix Akash

---
+++ coins/mainnet/uakt.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: 'uakt',
+  symbol: 'AKT',
+  name: 'Akash',
+  icon: 'https://station-assets.terra.money/img/coins/Akash.svg',
+  chains: ['akashnet-2'],
+  decimals: 6,
+}
