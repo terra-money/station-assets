1ebf401df32cecf81181aea44bb2f0d001c096bf
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-20 17:43:24 +0100

    Fix stargaze

--- chains/mainnet/Stargaze.js
+++ chains/mainnet/Stargaze.js
@@ -1,15 +1,17 @@
 module.exports = {
   chainID: 'stargaze-1',
-  lcd: 'https://rest.stargaze-apis.com/',
+  lcd: 'https://rest.stargaze-apis.com',
   gasAdjustment: 1.75,
   gasPrices: { ustars: 0 },
   prefix: 'stars',
   coinType: '118',
   baseAsset: 'ustars',
   name: 'Stargaze',
-  icon: 'https://www.notion.so/publicawesome/2634cdd76a5d44ba96a96af00f487c25?v=8ec845ada39c40ef94dc090b027d10fc&p=ec35a13969dc4268812a3ff23a99eded&pm=c',
+  icon: 'https://station-assets.terra.money/img/chains/Stargaze.png',
+  /*
   ibc: {
     fromTerra: 'None yet but can set up.',
     toTerra: 'None yet but can set up.',
   }
+  */
 }
