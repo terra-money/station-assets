3b1d715049dcd1cffa51616d890ead5b22311533
ItsRuwan <ItsRuwan@users.noreply.github.com>
2023-01-20 06:34:02 +0000

    Add

---
+++ chains/mainnet/Stargaze.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: 'stargaze-1',
+  lcd: 'https://rest.stargaze-apis.com/',
+  gasAdjustment: 1.75,
+  gasPrices: { ustars: 0 },
+  prefix: 'stars',
+  coinType: '118',
+  baseAsset: 'ustars',
+  name: 'Stargaze',
+  icon: 'https://www.notion.so/publicawesome/2634cdd76a5d44ba96a96af00f487c25?v=8ec845ada39c40ef94dc090b027d10fc&p=ec35a13969dc4268812a3ff23a99eded&pm=c',
+  ibc: {
+    fromTerra: 'None yet but can set up.',
+    toTerra: 'None yet but can set up.',
+  }
+}
