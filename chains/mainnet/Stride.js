c80130fcd1a89deec229f576ae1f787193c09737
shellvish <shellvish@users.noreply.github.com>
2023-01-23 19:58:17 +0000

    Add

---
+++ chains/mainnet/Stride.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: 'stride-1',
+  lcd: 'https://stride-fleet.main.stridenet.co/api',
+  gasAdjustment: 1.75,
+  gasPrices: { ustrd: 0 },
+  prefix: 'stride',
+  coinType: '118',
+  baseAsset: 'ustrd',
+  name: 'Stride',
+  icon: 'https://drive.google.com/file/d/112q1-5p2fP8aXnrpri2QCnMWGt4OIQ28/view?usp=sharing',
+  ibc: {
+    fromTerra: 'channel-46',
+    toTerra: 'channel-52',
+  }
+}
