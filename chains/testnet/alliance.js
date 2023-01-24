f4c6502d6faa42887810f7da9fa4a2df7bda4d05
emidev98 <49301655+emidev98@users.noreply.github.com>
2023-01-24 12:26:26 +0200

    feat: change alliance denom and prefix

--- chains/testnet/alliance.js
+++ chains/testnet/alliance.js
@@ -2,10 +2,10 @@ module.exports = {
   chainID: 'alliance-testnet-1',
   lcd: 'https://alliance-lcd.candeago.dev',
   gasAdjustment: 2.5,
-  gasPrices: { stake: 0.0 },
-  prefix: 'alliance',
+  gasPrices: { ally: 0.0 },
+  prefix: 'ally',
   coinType: '118',
-  baseAsset: 'stake',
+  baseAsset: 'ally',
   name: 'Alliance',
   icon: 'https://station-assets.terra.money/img/chains/Alliance.png',
 }
