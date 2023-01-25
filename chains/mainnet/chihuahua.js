754fb5af312ffe1b8c809849e5b9be6d40b9a6da
Zdeadex <51711900+zdeadex@users.noreply.github.com>
2023-01-25 15:01:27 +0100

    Update chihuahua.js

--- chains/mainnet/chihuahua.js
+++ chains/mainnet/chihuahua.js
@@ -2,7 +2,7 @@ module.exports = {
   chainID: 'chihuahua-1',
   lcd: 'https://api.chihuahua.wtf',
   gasAdjustment: 1.75,
-  gasPrices: { uhuahua: 1 },
+  gasPrices: { uhuahua: 1000000 },
   prefix: 'chihuahua',
   coinType: '118',
   baseAsset: 'uhuahua',
