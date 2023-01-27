f71a088906aea2e5f8465e6b5e9480c72f594412
masterfung <masterfung@users.noreply.github.com>
2023-01-27 16:13:19 +0000

    Add

---
+++ chains/Evmos.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: 'evmos_9001-2',
+  lcd: 'evmos-lcd.stakely.io',
+  gasAdjustment: 1.75,
+  gasPrices: { aevmos: 25000000000 },
+  prefix: 'evmos',
+  coinType: 'null',
+  baseAsset: 'aevmos',
+  name: 'Evmos',
+  icon: 'ttps://raw.githubusercontent.com/cosmos/chain-registry/master/evmos/images/evmos.svg',
+  ibc: {
+    fromTerra: 'channel-51',
+    toTerra: 'channel-6',
+  }
+}
