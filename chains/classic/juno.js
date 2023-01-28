763d82b1c385830025929fd9d23522c47ad06422
Erfan <41344995+flydeoo@users.noreply.github.com>
2023-01-28 12:33:54 +0330

    Create juno.js

---
+++ chains/classic/juno.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: 'juno-1',
+  lcd: 'https://lcd-juno.mib.tech',
+  gasAdjustment: 1.75,
+  gasPrices: { ujuno: 0.0025 },
+  prefix: 'juno',
+  coinType: '118',
+  baseAsset: 'ujuno',
+  name: 'Juno',
+  icon: 'https://station-assets.terra.money/img/chains/Juno.svg',
+  ibc: {
+    fromTerra: 'channel-20',
+    toTerra: 'channel-27',
+  },
+}
