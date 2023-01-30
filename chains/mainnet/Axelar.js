0b618c3a8f003551ddf3570ddc9cae7acf6e2377
jamesfriel <jamesfriel@users.noreply.github.com>
2023-01-30 21:09:31 +0000

    Add

---
+++ chains/mainnet/Axelar.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: 'axelar-dojo-1',
+  lcd: 'https://docs.axelar.dev/resources',
+  gasAdjustment: 1.75,
+  gasPrices: { uaxl: 0.007 },
+  prefix: 'axelar',
+  coinType: '118',
+  baseAsset: 'uaxl',
+  name: 'Axelar',
+  icon: 'https://drive.google.com/drive/u/1/folders/1FrYCAfilOWkunf3-l0MAwJYbfkcxYc9h',
+  ibc: {
+    fromTerra: 'channel-6',
+    toTerra: 'channel-11',
+  }
+}
