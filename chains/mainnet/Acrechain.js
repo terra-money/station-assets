4df296ab76b629cb061faf0e6047710d965ad442
gophereth <gophereth@users.noreply.github.com>
2023-02-03 13:15:38 +0000

    Add

---
+++ chains/mainnet/Acrechain.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: 'acre_9052-1',
+  lcd: 'https://lcd-acre.synergynodes.com/',
+  gasAdjustment: 1.75,
+  gasPrices: { aacre: 10000000000 },
+  prefix: 'acre',
+  coinType: '60',
+  baseAsset: 'aacre',
+  name: 'Acrechain',
+  icon: 'https://drive.google.com/file/d/1L34mBMHrfsCRyuu3XYQYZK6-rA8PG4qn/view',
+  ibc: {
+    fromTerra: 'channel-77',
+    toTerra: 'channel-9',
+  }
+}
