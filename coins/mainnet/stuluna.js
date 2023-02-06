165f5fd21649891c158373a9b7547adcc8c3a7e9
vish-stride <vishal@stridelabs.co>
2023-02-06 15:56:20 -0500

    Add stLUNA

---
+++ coins/mainnet/stuluna.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: 'stuluna',
+  symbol: 'stLUNA',
+  name: 'stLUNA',
+  icon: 'https://raw.githubusercontent.com/cosmos/chain-registry/master/stride/images/stluna.png',
+  chains: ['stride-1'],
+  decimals: 6,
+}
