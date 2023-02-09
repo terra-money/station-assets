5acd969f701fcaa13d889f1a5b81cba726a170fb
0xbobloblaw <0xbobloblaw@users.noreply.github.com>
2023-02-09 23:37:16 +0000

    Add

---
+++ chains/testnet/Migaloo.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: 'narwhal-1',
+  lcd: 'https://whitewhale-testnet-api.polkachu.com/',
+  gasAdjustment: 1.75,
+  gasPrices: { uwhale: 0 },
+  prefix: 'migaloo',
+  coinType: '118',
+  baseAsset: 'uwhale',
+  name: 'Migaloo',
+  icon: 'https://whitewhale.money/migaloo.svg',
+  ibc: {
+    fromTerra: 'NA',
+    toTerra: 'NA',
+  }
+}
