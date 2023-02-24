537b7e90c33c64ecd2804d0c5686d5e704707e26
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-02-24 17:00:37 +0100

    Switch back to TFL LCD

--- chains/classic/terra.js
+++ chains/classic/terra.js
@@ -1,6 +1,6 @@
 module.exports = {
   chainID: 'columbus-5',
-  lcd: 'https://terra-classic-lcd.publicnode.com',
+  lcd: 'https://columbus-lcd.terra.dev',
   gasAdjustment: 3,
   gasPrices: {
     uluna: 28.325,
