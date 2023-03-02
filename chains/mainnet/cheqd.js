85b2b54a0e0f145c0403247ab0b5e1113d129b23
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-03-02 16:57:32 +0100

    Fix image gasprices and endpoint

--- chains/mainnet/cheqd.js
+++ chains/mainnet/cheqd.js
@@ -1,13 +1,13 @@
 module.exports = {
   chainID: 'cheqd-mainnet-1',
-  lcd: 'rpc.cheqd.network',
+  lcd: 'https://api.cheqd.network',
   gasAdjustment: 1.75,
-  gasPrices: { ncheq: 50ncheq },
+  gasPrices: { ncheq: 50 },
   prefix: 'cheqd',
   coinType: '118',
   baseAsset: 'ncheq',
   name: 'cheqd',
-  icon: 'https://cheqd.io/hubfs/Logos/cheqd-Icon-Colour.svg',
+  icon: 'https://station-assets.terra.money/img/chains/Cheqd.svg',
   ibc: {
     fromTerra: 'channel-89',
     toTerra: 'channel-9',
