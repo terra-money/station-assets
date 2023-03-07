0db976c370527386fddf22f5831f9168bb429484
Jared <86672292+Jared-TFL@users.noreply.github.com>
2023-03-07 12:13:21 -0500

    Update cosmoshub.js

--- chains/mainnet/cosmoshub.js
+++ chains/mainnet/cosmoshub.js
@@ -1,6 +1,6 @@
 module.exports = {
   chainID: 'cosmoshub-4',
-  lcd: '',
+  lcd: 'https://cosmoshub-4.terra.dev:1317/node_info',
   gasAdjustment: 1.75,
   gasPrices: { uatom: 0.025 },
   prefix: 'cosmos',
