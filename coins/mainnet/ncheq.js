1744e21263625f240cdaf5fed17a44c87825cb4e
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-03-07 17:25:42 +0100

    Update CHEQ token

---
+++ coins/mainnet/ncheq.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token: 'ncheq',
+    symbol: 'CHEQ',
+    name: 'Cheq',
+    icon: 'https://station-assets.terra.money/img/coins/Cheqd.svg',
+    chains: ['cheqd-mainnet-1'],
+    decimals: 9,
+}
