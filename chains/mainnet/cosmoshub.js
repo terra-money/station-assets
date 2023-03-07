8ff3a65795aef0139d4128ccf5ca3a4264bcc39f
Jared <86672292+Jared-TFL@users.noreply.github.com>
2023-03-07 11:11:37 -0500

    Add CosmosHub to mainnet

---
+++ chains/mainnet/cosmoshub.js
@@ -0,0 +1,15 @@
+module.exports = {
+  chainID: 'cosmoshub-4',
+  lcd: '',
+  gasAdjustment: 1.75,
+  gasPrices: { uatom: 0.025 },
+  prefix: 'cosmos',
+  coinType: '118',
+  baseAsset: 'ATOM',
+  name: 'Cosmos Hub',
+  icon: '',
+  ibc: {
+    toTerra: 'channel-339',
+    fromTerra: 'channel-0',
+  },
+}
