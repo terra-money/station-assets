3faaa3134ea7cba22b3b66f17afe28666ecd26c7
Andrew-kerslake <andrew.kerslake@mrisoftware.com>
2023-03-14 12:44:53 +0000

    Create qamar.js

---
+++ chains/testnet/qamar.js
@@ -0,0 +1,13 @@
+module.exports = {
+    chainID: 'qamar',
+    lcd: 'https://rpc-dev.qamar.fi/',
+    gasAdjustment: 1.75,
+    gasPrices: { uqamar: 0.015 },
+    prefix: 'qamar',
+    coinType: '118',
+    baseAsset: 'uqamar',
+    name: 'Qamar',
+    icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
+    explorer: {}
+}
+
