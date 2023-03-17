e0bf714df3494cb8a9b2d4230414c0c00824f89b
ToxicLabs <120556880+ToxicLabs@users.noreply.github.com>
2023-03-17 11:34:40 +0000

    Update qamar.js

--- chains/testnet/qamar.js
+++ chains/testnet/qamar.js
@@ -8,64 +8,64 @@ module.exports = {
     baseAsset: 'uqamar',
     name: 'Qamar',
     icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
-    explorer: {}
+    explorer: {},
     tokens: [
     {
       token: 'uqamar',
       symbol: 'Qamar',
       name: 'Qamar',
-      icon: 'https://station-assets.terra.money/img/coins/Juno.svg',
+      icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
       decimals: 6,
     },
     {
       token: 'uwusdc',
       symbol: 'WUSDC',
       name: 'Wrapped USDC',
-      icon: 'https://station-assets.terra.money/img/coins/Juno.svg',
+      icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
       decimals: 6,
     },
     {
       token: 'uwusdt',
       symbol: 'WUSDT',
       name: 'Wrapped USDT',
-      icon: 'https://station-assets.terra.money/img/coins/Juno.svg',
+      icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
       decimals: 6,
     },
     {
       token: 'uwdai',
       symbol: 'WDAI',
       name: 'Wrapped DAI',
-      icon: 'https://station-assets.terra.money/img/coins/Juno.svg',
+      icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
       decimals: 6,
     },
     {
       token: 'uwbusd',
       symbol: 'WBUSD',
       name: 'Wrapped BUSD',
-      icon: 'https://station-assets.terra.money/img/coins/Juno.svg',
+      icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
       decimals: 6,
     },
     {
       token: 'uwbtc',
       symbol: 'WBTC',
       name: 'Wrapped Bitcoin',
-      icon: 'https://station-assets.terra.money/img/coins/Juno.svg',
+      icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
       decimals: 6,
     },
     {
       token: 'uqusd',
       symbol: 'QUSD',
       name: 'Qamar USD',
-      icon: 'https://station-assets.terra.money/img/coins/Juno.svg',
+      icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
       decimals: 6,
     },
     {
       token: 'uqbtc',
       symbol: 'QBTC',
       name: 'Qamar Bitcoin',
-      icon: 'https://station-assets.terra.money/img/coins/Juno.svg',
+      icon: 'https://assets.terra.money/icon/svg/TerraClassic.svg',
       decimals: 6,
     }
-  ],
+  ]
 }
 
