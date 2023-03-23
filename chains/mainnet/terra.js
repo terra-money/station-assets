846d5aaa00ccfe87c38c09264ce5cb61074f3f42
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-03-23 07:15:43 +0100

    Fix ampLuna image url

--- chains/mainnet/terra.js
+++ chains/mainnet/terra.js
@@ -62,7 +62,7 @@ module.exports = {
       token: 'terra1ecgazyd0waaj3g7l9cmy5gulhxkps2gmxu9ghducvuypjq68mq2s5lvsct',
       symbol: 'ampLUNA',
       name: 'ERIS Amplified LUNA',
-      icon: 'https://station-assets.terra.money/img/coins/ampLUNA.svg',
+      icon: 'https://station-assets.terra.money/img/coins/ampLuna.svg',
       decimals: 6,
     },
   ],
