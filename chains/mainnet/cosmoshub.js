0ba3a03bf1f00e2a8ce748ce7bf3e9e6cbf8b152
Mike <17463738+mwmerz@users.noreply.github.com>
2023-05-09 17:24:59 -0400

    re-enable CosmosHub

--- chains/mainnet/cosmoshub.js
+++ chains/mainnet/cosmoshub.js
@@ -1,7 +1,6 @@
 module.exports = {
   chainID: 'cosmoshub-4',
-  //lcd: 'https://cosmoshub-4.terra.dev',
-  lcd: '',
+  lcd: 'https://cosmoshub-4.terra.dev',
   gasAdjustment: 1.75,
   gasPrices: { uatom: 0.025 },
   prefix: 'cosmos',
