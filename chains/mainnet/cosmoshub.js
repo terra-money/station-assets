e0ddc73e9d1944452d592073231609c32b94bbb8
Mike <17463738+mwmerz@users.noreply.github.com>
2023-05-09 15:52:19 -0400

    temporarily disable cosmoshub
    temporarily disable cosmoshub lcd url

--- chains/mainnet/cosmoshub.js
+++ chains/mainnet/cosmoshub.js
@@ -1,6 +1,7 @@
 module.exports = {
   chainID: 'cosmoshub-4',
-  lcd: 'https://cosmoshub-4.terra.dev',
+  //lcd: 'https://cosmoshub-4.terra.dev',
+  lcd: '',
   gasAdjustment: 1.75,
   gasPrices: { uatom: 0.025 },
   prefix: 'cosmos',
