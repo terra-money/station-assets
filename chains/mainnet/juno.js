5fa3fb740e0c9bfbe183340d1141a54322019feb
Joshua Brigati <brigati.joshua@gmail.com>
2023-05-31 10:25:26 -0500

    Update juno.js
    Changes caps for ampJuno svg

--- chains/mainnet/juno.js
+++ chains/mainnet/juno.js
@@ -42,7 +42,7 @@ module.exports = {
       token: 'juno1a0khag6cfzu5lrwazmyndjgvlsuk7g4vn9jd8ceym8f4jf6v2l9q6d348a',
       symbol: 'ampJUNO',
       name: 'ERIS Amplified JUNO',
-      icon: 'https://station-assets.terra.money/img/coins/ampJUNO.svg',
+      icon: 'https://station-assets.terra.money/img/coins/ampJuno.svg',
       decimals: 6,
     },
   ],
