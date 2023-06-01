6f9b5ea5cdde5271dae53beb87d039355f9bb90e
Terence Lim <terencelimz@gmail.com>
2023-06-01 18:25:11 +0800

    Added assets for Kujira mainnet

--- chains/mainnet/kujira.js
+++ chains/mainnet/kujira.js
@@ -50,7 +50,7 @@ module.exports = {
     },
     {
       token:
-        'factory:kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7:uusk',
+        'factory/kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7/uusk',
       symbol: 'USK',
       name: 'USK',
       icon: 'https://station-assets.terra.money/img/coins/USK.svg',
@@ -58,11 +58,32 @@ module.exports = {
     },
     {
       token:
-        'factory:kujira1n3fr5f56r2ce0s37wdvwrk98yhhq3unnxgcqus8nzsfxvllk0yxquurqty:ampKUJI',
+        'factory/kujira1n3fr5f56r2ce0s37wdvwrk98yhhq3unnxgcqus8nzsfxvllk0yxquurqty/ampKUJI',
       symbol: 'ampKUJI',
       name: 'ERIS Amplified KUJI',
       icon: 'https://station-assets.terra.money/img/coins/ampKUJI.svg',
       decimals: 6,
     },
+    {
+      token: 'factory/kujira1swkuyt08z74n5jl7zr6hx0ru5sa2yev5v896p6/local',
+      symbol: 'LOCAL',
+      name: 'Local Money',
+      icon: 'https://localmoney.io/local-logo-dark.png',
+      decimals: 6
+    },
+    {
+        token: 'factory/kujira13y8hs83sk0la7na2w5g5nzrnjjpnkvmd7e87yd35g8dcph7dn0ksenay2a/ulp',
+        symbol: 'LP KUJI-ATOM',
+        name: 'LP KUJI-ATOM',
+        icon: '',
+        decimals: 6
+    },
+    {
+        token: 'factory/kujira1643jxg8wasy5cfcn7xm8rd742yeazcksqlg4d7/umnta',
+        symbol: 'MNTA',
+        name: 'MNTA',
+        icon: '',
+        decimals: 6
+    }
   ],
 }
