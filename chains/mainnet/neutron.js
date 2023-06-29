ee7eee1cff04d5e023f5e02ab841d1c06ac8e53f
plubber <er.h.gorski@gmail.com>
2023-06-29 11:29:36 -0400

    wip: neutron addition

--- chains/mainnet/neutron.js
+++ chains/mainnet/neutron.js
@@ -1,12 +1,13 @@
 module.exports = {
     chainID: 'neutron-1',
-    lcd: 'https://lcd-neutron.whispernode.com',
+    lcd: 'https://rest-kralum.neutron-1.neutron.org',
     gasAdjustment: 1.75,
     gasPrices: { untrn: 0.01 },
     prefix: 'neutron',
     coinType: '118',
     baseAsset: 'untrn',
     name: 'Neutron',
+    stakingDisabled: true,
     icon: process.env.CF_PAGES_URL + '/img/chains/Neutron.svg',
     ibc: {
       fromTerra: 'channel-167',
@@ -26,6 +27,20 @@ module.exports = {
         icon: process.env.CF_PAGES_URL + '/img/coins/Neutron.svg',
         decimals: 6,
       },
-    ],
+      {
+        token: 'uatom',
+        symbol: 'ATOM',
+        name: 'Cosmos',
+        icon: process.env.CF_PAGES_URL + '/img/coins/Cosmos.svg',
+        decimals: 6,
+      },
+      {
+        token: 'uusdc',
+        symbol: 'USDC',
+        name: 'usdc',
+        icon: process.env.CF_PAGES_URL + '/img/coins/axlUSDC.svg',
+        decimals: 6,
+      },
+    ]
   }
   
