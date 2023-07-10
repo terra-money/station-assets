9fab95fd3fd5dec49a1db2bcd7b76bc4d059a5f1
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-07-10 20:25:21 +0200

    fix kujira factory tokens

--- chains/mainnet/kujira.js
+++ chains/mainnet/kujira.js
@@ -62,7 +62,7 @@ module.exports = {
     },
     {
       token:
-        'factory:kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7:uusk',
+        'factory/kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7/uusk',
       symbol: 'USK',
       name: 'USK',
       icon: process.env.CF_PAGES_URL + '/img/coins/USK.svg',
@@ -70,14 +70,14 @@ module.exports = {
     },
     {
       token:
-        'factory:kujira1n3fr5f56r2ce0s37wdvwrk98yhhq3unnxgcqus8nzsfxvllk0yxquurqty:ampKUJI',
+        'factory/kujira1n3fr5f56r2ce0s37wdvwrk98yhhq3unnxgcqus8nzsfxvllk0yxquurqty/ampKUJI',
       symbol: 'ampKUJI',
       name: 'ERIS Amplified KUJI',
       icon: process.env.CF_PAGES_URL + '/img/coins/ampKUJI.svg',
       decimals: 6,
     },
     {
-      token: 'factory:kujira1swkuyt08z74n5jl7zr6hx0ru5sa2yev5v896p6:local',
+      token: 'factory/kujira1swkuyt08z74n5jl7zr6hx0ru5sa2yev5v896p6/local',
       symbol: 'LOCAL',
       name: 'Local Money',
       icon: process.env.CF_PAGES_URL + '/img/coins/Local.png',
@@ -85,14 +85,14 @@ module.exports = {
     },
     {
       token:
-        'factory:kujira13y8hs83sk0la7na2w5g5nzrnjjpnkvmd7e87yd35g8dcph7dn0ksenay2a:ulp',
+        'factory/kujira13y8hs83sk0la7na2w5g5nzrnjjpnkvmd7e87yd35g8dcph7dn0ksenay2a/ulp',
       symbol: 'LP KUJI-ATOM',
       name: 'LP KUJI-ATOM',
       icon: '',
       decimals: 6,
     },
     {
-      token: 'factory:kujira1643jxg8wasy5cfcn7xm8rd742yeazcksqlg4d7:umnta',
+      token: 'factory/kujira1643jxg8wasy5cfcn7xm8rd742yeazcksqlg4d7/umnta',
       symbol: 'MNTA',
       name: 'MNTA',
       icon: '',
