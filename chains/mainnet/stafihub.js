3a3c579dbef692f302041e8a04a72766b273b7de
Tore19 <289649077@qq.com>
2023-08-01 09:49:01 +0800

    Add StaFiHub to mainnet

---
+++ chains/mainnet/stafihub.js
@@ -0,0 +1,66 @@
+module.exports = {
+  chainID: 'stafihub-1',
+  lcd: 'https://public-rest-rpc1.stafihub.io',
+  gasAdjustment: 1.75,
+  gasPrices: { ufis: 0.02 },
+  prefix: 'stafi',
+  coinType: '118',
+  baseAsset: 'ufis',
+  name: 'StaFiHub',
+  icon: process.env.CF_PAGES_URL + '/img/chains/StaFiHub.png',
+  ibc: {
+    fromTerra: 'channel-204',
+    toTerra: 'channel-9',
+  },
+  channels: {
+    'phoenix-1': 'channel-9',
+    'cosmoshub-4': 'channel-0',
+    'irishub-1': 'channel-3',
+    'chihuahua-1': 'channel-4',
+    'carbon-1': 'channel-5',
+    'kaiyo-1': 'channel-6',
+  },
+  explorer: {
+    address: 'https://www.mintscan.io/stafi/accounts/{}',
+    tx: 'https://www.mintscan.io/stafi/transactions/{}',
+    validator: 'https://www.mintscan.io/stafi/validators/{}',
+    block: 'https://www.mintscan.io/stafi/blocks/{}',
+  },
+  tokens: [
+    {
+      token: 'ufis',
+      symbol: 'FIS',
+      name: 'FIS',
+      icon: process.env.CF_PAGES_URL + '/img/coins/FIS.png',
+      decimals: 6,
+    },
+    {
+      token: 'uratom',
+      symbol: 'rATOM',
+      name: 'rATOM',
+      icon: process.env.CF_PAGES_URL + '/img/coins/rATOM.svg',
+      decimals: 6,
+    },
+    {
+      token: 'uriris',
+      symbol: 'rIRIS',
+      name: 'rIRIS',
+      icon: process.env.CF_PAGES_URL + '/img/coins/rIRIS.svg',
+      decimals: 6,
+    },
+    {
+      token: 'urhuahua',
+      symbol: 'rHUAHUA',
+      name: 'rHUAHUA',
+      icon: process.env.CF_PAGES_URL + '/img/coins/rHUAHUA.svg',
+      decimals: 6,
+    },
+    {
+      token: 'urswth',
+      symbol: 'rSWTH',
+      name: 'rSWTH',
+      icon: process.env.CF_PAGES_URL + '/img/coins/rSWTH.svg',
+      decimals: 6,
+    }
+  ],
+}
