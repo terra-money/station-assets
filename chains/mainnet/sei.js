182dd5de2ba316e0e71567e10a88f47af46ffabb
plubber <er.h.gorski@gmail.com>
2023-08-15 15:32:39 -0400

    add: sei

---
+++ chains/mainnet/sei.js
@@ -0,0 +1,29 @@
+module.exports = {
+    chainID: 'pacific-1',
+    lcd: 'https://sei-api.polkachu.com/',
+    gasAdjustment: 2,
+    gasPrices: { usei: 0.1 },
+    prefix: 'sei',
+    coinType: '118',
+    baseAsset: 'usei',
+    name: 'Sei',
+    icon: process.env.CF_PAGES_URL + '/img/chains/sei.svg',
+    ibc: {},
+    channels: {},
+    explorer: {
+      address: 'https://sei.explorers.guru/account/{}',
+      tx: 'https://sei.explorers.guru/transaction/{}',
+      validator: 'https://sei.explorers.guru/validator/{}',
+      block: 'https://sei.explorers.guru/block/{}',
+    },
+    tokens: [
+      {
+        token: 'usei',
+        symbol: 'SEI',
+        name: 'Test Sei',
+        icon: process.env.CF_PAGES_URL + '/img/coins/sei.svg',
+        decimals: 6,
+      },
+    ],
+  }
+  
