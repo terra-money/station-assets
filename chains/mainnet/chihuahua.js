f397ac7a3ead492b36eb2a9b0f9c85567dfcaa14
Terence Lim <terencelimz@gmail.com>
2023-09-04 10:13:07 +0800

    Updated gas step for huahua

--- chains/mainnet/chihuahua.js
+++ chains/mainnet/chihuahua.js
@@ -2,7 +2,7 @@ module.exports = {
   chainID: 'chihuahua-1',
   lcd: 'https://api.chihuahua.wtf',
   gasAdjustment: 2,
-  gasPrices: { uhuahua: 250 },
+  gasPrices: { uhuahua: 1250 },
   prefix: 'chihuahua',
   coinType: '118',
   baseAsset: 'uhuahua',
