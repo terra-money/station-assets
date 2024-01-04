39803f6c224f912dc24a038541de3d7dc4ff9fbd
Terence Lim <terencelimz@gmail.com>
2024-01-05 00:53:32 +0800

    Increased gasadjustment for terra

--- chains/mainnet/terra.js
+++ chains/mainnet/terra.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainID: 'phoenix-1',
   lcd: 'https://phoenix-lcd.terra.dev',
-  gasAdjustment: 1.2,
+  gasAdjustment: 1.75,
   gasPrices: { uluna: 0.15 },
   prefix: 'terra',
   coinType: '330',
