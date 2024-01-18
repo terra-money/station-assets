58a38056b31211a9dd2b50c84ac175ada2a4a024
Terence Lim <terencelimz@gmail.com>
2024-01-18 15:30:40 +0800

    Updated osmosis lcd

--- chains/mainnet/osmosis.js
+++ chains/mainnet/osmosis.js
@@ -1,6 +1,7 @@
 module.exports = {
   chainID: 'osmosis-1',
-  lcd: 'https://lcd-osmosis.tfl.foundation',
+  // lcd: 'https://lcd-osmosis.tfl.foundation',
+  lcd: 'https://osmosis-api.polkachu.com',
   gasAdjustment: 1.5,
   gasPrices: {
     uosmo: {
