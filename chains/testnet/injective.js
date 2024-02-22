166a94179f93194e658cc5b61be409a9c227230d
tuky191 <michal@terra.money>
2024-02-22 22:05:46 +0100

    update injective testnet url

--- chains/testnet/injective.js
+++ chains/testnet/injective.js
@@ -1,6 +1,6 @@
 module.exports = {
   chainID: 'injective-888',
-  lcd: 'https://k8s.testnet.lcd.injective.network',
+  lcd: 'https://testnet.sentry.lcd.injective.network/',
   gasAdjustment: 1.75,
   gasPrices: { inj: 1000000000 },
   prefix: 'inj',
@@ -9,7 +9,7 @@ module.exports = {
   name: 'Injective',
   icon: process.env.CF_PAGES_URL + '/img/chains/Injective.svg',
   channels: {
-    'indigo-1': 'channel-151'
+    'indigo-1': 'channel-151',
   },
   explorer: {
     address: 'https://testnet.explorer.injective.network/account/{}',
